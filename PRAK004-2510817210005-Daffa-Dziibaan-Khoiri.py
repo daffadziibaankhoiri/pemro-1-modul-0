fd5f558f8c4cf638f29e0bae24aa3713f7835412
Daffa Dziiba'an Khoiri <155513300+daffadziibaankhoiri@users.noreply.github.com>
2025-09-24 09:32:10 +0800

    Upload Python

---
+++ PRAK004-2510817210005-Daffa-Dziibaan-Khoiri.py
@@ -0,0 +1,24 @@
+
+
+nama = "Daffa Dziiba'an Khoiri"
+nim = "2510817210005"
+penentu_panjang = len(nama) > len(nim)
+panjang_batas = 0
+
+if(penentu_panjang):
+    
+    panjang_batas = len(nama) + 14
+    
+else: 
+    panjang_batas = len(str(nim)) + 14
+
+
+nama_line = f"#{nama:^{panjang_batas - 2}}#"
+nim_line = f"#{nim:^{panjang_batas - 2}}#"
+
+print("#" * panjang_batas)
+print("#", " " * (panjang_batas-4), "#")
+print(nama_line)
+print(nim_line)
+print("#", " " * (panjang_batas-4), "#")
+print("#" * panjang_batas)
